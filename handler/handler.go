4c3c29ce2177f74729527f641598634ffb621d0a
lucagez <lucagesmundo@yahoo.it>
2021-11-05 00:55:54 +0100

    add dummy command

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,22 @@
 package handler
 
 import (
+    "bufio"
+    "bytes"
     "fmt"
     "net/http"
+    "os/exec"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "This wa definitely updated \n")
+    cmd := exec.Command("deno", "eval", "console.log('Ciaooo')")
+
+    var buf bytes.Buffer
+    out := bufio.NewWriter(&buf)
+    cmd.Stdout = out
+    cmd.Stderr = out
+    cmd.Run()
+
+    fmt.Fprintf(w, buf.String())
 }
 
