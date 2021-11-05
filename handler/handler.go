1cec2b2bb5cae97c1225e3b989b3b13b8171206d
lucagez <lucagesmundo@yahoo.it>
2021-11-05 01:26:22 +0100

    add init

--- handler/handler.go
+++ handler/handler.go
@@ -5,11 +5,20 @@ import (
     "bytes"
     "fmt"
     "net/http"
+    "os"
     "os/exec"
 )
 
+func init() {
+    os.Setenv("DENO_INSTALL", ".")
+
+    cmd := exec.Command("sh", "install.sh")
+    cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+    cmd.Run()
+}
+
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-    cmd := exec.Command("/opt/render/.deno/bin/deno", "eval", "console.log('Ciaooo')")
+    cmd := exec.Command("bin/deno", "eval", "console.log('Ciaooo')")
 
     var buf bytes.Buffer
     out := bufio.NewWriter(&buf)
@@ -17,8 +26,11 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
     cmd.Stderr = out
     err := cmd.Run()
 
-    fmt.Fprintf(w, err.Error())
-    fmt.Fprintf(w, "\n")
+    if err != nil {
+        fmt.Fprintf(w, err.Error())
+        fmt.Fprintf(w, "\n")
+    }
+
     fmt.Fprintf(w, buf.String())
 }
 
