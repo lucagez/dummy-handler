7f1198d7399beac18260ee2d9b24e8e50e2c4581
lucagez <lucagesmundo@yahoo.it>
2021-11-05 01:00:09 +0100

    err handling

--- handler/handler.go
+++ handler/handler.go
@@ -9,14 +9,16 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-    cmd := exec.Command("deno", "eval", "console.log('Ciaooo')")
+    cmd := exec.Command("/opt/render/.deno/bin/deno", "eval", "console.log('Ciaooo')")
 
     var buf bytes.Buffer
     out := bufio.NewWriter(&buf)
     cmd.Stdout = out
     cmd.Stderr = out
-    cmd.Run()
+    err := cmd.Run()
 
+    fmt.Fprintf(w, err.Error())
+    fmt.Fprintf(w, "\n")
     fmt.Fprintf(w, buf.String())
 }
 
